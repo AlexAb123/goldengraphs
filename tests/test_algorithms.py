c81c2b5015bbd488d2b3ad51df35d16e6e0a407d
AlexAb123 <alex.abdulhayoglu@gmail.com>
2025-11-24 13:52:29 -0500

    refactor

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -4,8 +4,10 @@ from goldengraphs import a_star, dijkstra, dijkstra_all_paths
 def test_dijkstra() -> None:
     pass
 
+
 def test_dijkstra_all_paths() -> None:
     pass
+
 
 def test_a_star() -> None:
     pass
